701b5815ffbf94cf6b9f7fc73662a48e75925dfa
Omar Tarek <omar.t4352@gmail.com>
2021-09-21 00:04:14 +0200

    Update pwm.c

--- MCAL/pwm.c
+++ MCAL/pwm.c
@@ -4,3 +4,12 @@
  * Created: 9/20/2021 11:27:22 PM
  *  Author: Omar
  */ 
+void PWM_init(PWM_config_t* configs)
+{
+	//set pwm configurations and registers
+}
+
+void PWM_start(EN_dutyCycle_t dutyCycle)
+{
+	//set dutycycle bits in registers
+}
